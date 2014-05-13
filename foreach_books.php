a3057e6ace5db06bd55b925a310a4f1e4f84da16
Daniel Jimenez <danieljimenez@Daniels-MacBook-Air.local>
2014-05-13 17:11:43 -0500

    books list with foreach array and multi-dimentional arrays

--- foreach_books.php
+++ foreach_books.php
@@ -1,24 +1,18 @@
 <?php
 
-$books = array(
-    'The Hobbit' => array(
-        'published' => 1937,
-        'author' => 'J. R. R. Tolkien',
-        'pages' => 310
-    ),
-    'Game of Thrones' => array(
-        'published' => 1996,
-        'author' => 'George R. R. Martin',
-        'pages' => 835
-    ),
-    'The Catcher in the Rye' => array(
-        'published' => 1951,
-        'author' => 'J. D. Salinger',
-        'pages' => 220
-    ),
-    'A Tale of Two Cities' => array(
-        'published' => 1859,
-        'author' => 'Charles Dickens',
-        'pages' => 544
-    )
-);
+$books = 
+[
+    'The Hobbit' =>             ['published' => 1937,'author' => 'J. R. R. Tolkien','pages' => 310],
+    'Game of Thrones' =>        ['published' => 1996,'author' => 'George R. R. Martin','pages' => 835],
+    'The Catcher in the Rye' => ['published' => 1951,'author' => 'J. D. Salinger','pages' => 220],
+    'A Tale of Two Cities' =>   ['published' => 1859,'author' => 'Charles Dickens','pages' => 544]
+];
+
+//Construct a loop that iterates through each book and then each book's keys and values. 
+//Have it output the book's title, then list the key value pairs for the data about each book.
+
+foreach ($books as $book => $details) 
+{   
+    echo $book . " was published in " . $details['published'] . " by " . 
+    $details['author'] . " and has " . $details['pages'] . " pages." . PHP_EOL;
+}
