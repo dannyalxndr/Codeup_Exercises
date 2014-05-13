11d85e7303362bf1a7a1cfb4da5235d8803145f7
Daniel Jimenez <danieljimenez@Daniels-MacBook-Air.local>
2014-05-13 10:48:36 -0500

    added comments for logic

--- for.php
+++ for.php
@@ -1,31 +1,40 @@
 <?php
 
+// prompt asks for your name
 fwrite (STDOUT, 'What is your name? ');
 
 $name = trim(fgets(STDIN));
 
+// prompt asks you for your starting number
 fwrite(STDOUT, "Hello there $name. Choose a starting number..\n");
 
 $number1 = trim(fgets(STDIN));
 
+// condition for number 1 to be numeric. if not, then program exits
 if (!is_numeric($number1)) 
 {
 	echo "I need a number, chief.\n";
 	exit(1);
 }
 
+// prompt asks you for your ending number
 fwrite(STDOUT, "Great! Now choose an ending number..\n");
 
 $number2 = trim(fgets(STDIN));
+
+// condition for number 2 to be numeric. if not, then program exits
 if (!is_numeric($number2)) 
 {
 	echo "I need a number, chief.\n";
 	exit(1);
 }
 
+// prompt asks you for your increment
 fwrite(STDOUT, "Choose your increment." . PHP_EOL);
 
 $increment = trim(fgets(STDIN));
+
+// condition for there being no input, if not numeric.
 if ($increment == NULL) 
 {
 	$increment = 1;
@@ -35,6 +44,8 @@ elseif (!is_numeric($increment))
 	echo "I need a number, chief.\n";
 	exit(1);
 }
+
+// conditions if number 1 is greater than number 2
 if ($number1 < $number2) 
 { 
 	define('LOWEND', (int)$number1);
@@ -51,6 +62,7 @@ else
 	exit(1);
 }
 
+// prompt gets ready to give you the results
 fwrite(STDOUT, "Here you go..\n");
 for ( $number1 ; $number1 <= $number2 ; $number1 = $number1 + $increment ) 
 {
