f2327c9d71c01520114ba2b90605937edab1b75b
Daniel Jimenez <danieljimenez@Daniels-MacBook-Air.local>
2014-05-13 15:27:46 -0500

    minor changes

--- foreach.php
+++ foreach.php
@@ -4,48 +4,76 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 
 
 //echo's out values if the are scalar
-foreach ($things as $thing)
+// foreach ($things as $thing) {
+//     if (is_int($thing)) {
+//          echo "{$thing} is an interger\n";
+//     } 
+//     elseif (is_float($thing)) {
+//         echo "{$thing} is a float\n";
+//     } 
+//     elseif (is_bool($thing)) {
+//     	echo "{$thing} is a boolean\n";
+//     }
+//     elseif (is_array($thing)) {
+//     	echo "Value is an array\n";
+//     }
+//     elseif (is_null($thing)) {
+//     	echo "{$thing} is null\n";
+//     }
+//     elseif (is_string($thing)) {
+//      	echo "{$thing} is a string\n";
+//     if (is_scalar($thing)) {
+//     	echo "{$thing} \n";
+//      }
+// }
+
+
+
+// echo's out every value in the array as well
+  //   if (is_scalar($thing)) {
+  //     	echo "{$thing} \n";
+  //   }
+  //   elseif (is_null($thing)) {
+  //    	echo "{$thing} \n";
+  //   }
+  //   elseif (is_array ($thing)) {
+  //    	foreach ($thing as $inside_array) {
+  //    		echo "$inside_array \n"; 
+  //   	}
+  //   }
+   
+  // }
+
+
+
+foreach ($things as $thing) 
 {
-	if (is_scalar($thing))
-	{
-		echo $thing . PHP_EOL;
-	}	
+    if (is_int($thing)) 
+    {
+        echo "{$thing} is an interger\n";
+    } 
+    elseif (is_float($thing)) 
+    {
+        echo "{$thing} is a float\n";
+    } 
+    elseif (is_bool($thing)) 
+    {
+    	echo "{$thing} is a boolean\n";
+    }
+    elseif (is_array($thing)) 
+    {
+    	foreach ($thing as $item)
+    	{
+    		echo $item . ",";
+    	}
+    	echo "...is an array\n";
+    }
+    elseif (is_null($thing)) 
+    {
+    	echo "{$thing} is null\n";
+    }
+    elseif (is_string($thing)) 
+    {
+    	echo "{$thing} is a string\n";
+    }
 }
-
-
-
-// echo's out every value in the array.
-// foreach ($things as $thing)
-// {
-// 	echo $thing . PHP_EOL;
-// }
-
-
-
-// foreach ($things as $thing)
-// {
-// 	if (is_integer($thing))
-// 	{
-// 		echo "$thing is an integer.\n";
-// 	}
-// 	elseif (is_string($thing))
-// 	{
-// 		echo "$thing is a string.\n";
-// 	}
-// 	elseif (is_float($thing))
-// 	{
-// 		echo "$thing is a float.\n";
-// 	}
-// 	elseif (is_bool($thing))
-// 	{
-// 		echo "$thing is a boolean.\n";
-// 	}
-// 	elseif (is_array($thing))
-// 	{
-// 		echo "$thing is an array.\n";
-// 	}
-// 	elseif (is_null($thing))
-// 	{
-// 		echo "$thing is null.\n";
-// 	}
-// }
