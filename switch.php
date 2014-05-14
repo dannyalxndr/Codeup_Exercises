3e7ddc72115b4d813cdca72617938800b166151c
Daniel Jimenez <danieljimenez@Daniels-MacBook-Air.local>
2014-05-14 13:22:00 -0500

    switch.php file that tells the day of the week

--- switch.php
+++ switch.php
@@ -5,28 +5,28 @@ date_default_timezone_set('America/Chicago');
 
 // Get Day of Week as number
 // 1 (for Monday) through 7 (for Sunday)
-$day_of_week = 'friday';
+$day_of_week = date('3');
 
 switch($day_of_week) {
-    case 'monday':
+    case '1':
         echo 'monday' . PHP_EOL;
         break;
-  	case 'tuesday':
+  	case '2':
         echo 'tuesday' . PHP_EOL;
         break;
-    case 'wednesday':
+    case '3':
         echo 'wednesday' . PHP_EOL;
         break;
-    case 'thursday':
+    case '4':
         echo 'thursday' . PHP_EOL;
         break;
-    case 'friday':
+    case '5':
         echo 'friday' . PHP_EOL;
         break;
-    case 'saturday':
+    case '6':
         echo 'saturday' . PHP_EOL;
         break;
-    case 'sunday':
+    case '7':
         echo 'sunday' . PHP_EOL;
         break;
 }
