3bf2fd9e6c0479f0b0f412be6ca0316c80e504bd
Daniel Jimenez <danieljimenez@Daniels-MacBook-Air.local>
2014-05-16 10:47:45 -0500

    added serial function stored in variable

--- internal_functions.php
+++ internal_functions.php
@@ -1,10 +1,22 @@
 <?php
 
+
+
+// Create a function that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
+
+// TEST: If var $nothing is set, display '$nothing is SET'
+
+// TEST: If var $nothing is empty, display '$nothing is EMPTY'
+
+// TEST: If var $something is set, display '$something is SET'
+
+// Serialize the array $array, and output the results
+
+// Unserialize the array $array, and output the results
+
 $nothing = NULL;
 $something = '';
 $array = array(1,2,3);
-
-// Create a function that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
 
 function check($x)
 {
@@ -20,12 +32,23 @@ function check($x)
 
 echo check($nothing);
 
-// TEST: If var $nothing is set, display '$nothing is SET'
+// created a serialize function and variable storing the value to $serial
+function serial($x)
+{
+	return serialize($x) . PHP_EOL;
+}
 
-// TEST: If var $nothing is empty, display '$nothing is EMPTY'
+$serial = serial($array);
 
-// TEST: If var $something is set, display '$something is SET'
+echo $serial;
 
-// Serialize the array $array, and output the results
+var_dump(unserialize($serial));
 
-// Unserialize the array $array, and output the results
+
+
+
+
+
+
+
+
