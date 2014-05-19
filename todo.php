5c71d4f20c4e90487baab473779d8a8aeffbdd24
Daniel Jimenez <danieljimenez@Daniels-MacBook-Air.local>
2014-05-19 11:14:44 -0500

    todo list sort function.

--- todo.php
+++ todo.php
@@ -8,8 +8,7 @@ function list_items($list)
 
     foreach ($list as $key => $value)
     {
-        $key++;
-        $result .= "[{$key}] {$value}\n";  
+        $result .= "[" . ($key + 1) . "] {$value}\n";  
     }
     return $result;
 }
@@ -20,10 +19,35 @@ function get_input($upper = false)
     return $upper ? strtoupper($result) : $result;
 }
 
+function sort_menu($items)
+{
+    echo "(A)-Z, (Z)-A, (O)rder entered, (R)everse order entered: ";
+    $input = get_input(true); 
+
+    // sort the $items
+    switch ($input) 
+    {
+        case 'A':
+            sort($items);
+            break;
+        case 'Z':
+            rsort($items);
+            break;
+        case 'O':
+            asort($items);
+            break;
+        case 'R':
+            arsort($items);
+            break;
+    }
+
+    return $items;   
+}
+
 do 
 {
     echo list_items($items);
-    echo '(N)ew item, (R)emove item, (Q)uit : ';
+    echo '(N)ew item, (R)emove item, (S)ort, (Q)uit : ';
     $input = get_input(true);
 
     if ($input == 'N') 
@@ -38,6 +62,10 @@ do
         $key--;
         unset($items[$key]);
         $items = array_values($items);
+    }
+    elseif ($input == 'S')
+    {
+        $items = sort_menu($items);
     }
 } 
 while ($input != 'Q');
