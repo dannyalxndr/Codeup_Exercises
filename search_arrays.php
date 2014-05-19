3052180ad50131e4caba71bb30df1082b3927fcd
Daniel Jimenez <danieljimenez@Daniels-MacBook-Air.local>
2014-05-19 15:27:06 -0500

    working array comparison function

--- search_arrays.php
+++ search_arrays.php
@@ -6,8 +6,7 @@ $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
 
-
-function search_results($needle, $haystack)
+function array_has_value($needle, $haystack)
 {
 
 	if(array_search($needle, $haystack) === false)
@@ -20,4 +19,31 @@ function search_results($needle, $haystack)
 	}
 }
 
-var_dump(search_results('Michael', $names));
+// var_dump(array_has_value('Michael', $names));
+
+// Create a function to compare 2 arrays that returns the number of values in common between the arrays. Use the 2 example arrays and make sure your solution uses array_search().
+
+
+function array_common_count($array1, $array2)
+{
+	$count = 0;
+
+	foreach($array1 as $value)
+	{
+		if(array_has_value($value, $array2))
+		{
+			$count++;
+		}
+	}
+	return $count;
+}
+
+var_dump(array_common_count($names, $compare));
+
+
+
+
+
+
+
+
