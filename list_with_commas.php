b80b87d50043a75e47b0f422aa8ee5a7b65f4374
Daniel Jimenez <danieljimenez@Daniels-MacBook-Air.local>
2014-05-20 14:30:28 -0500

    made sorting optional in the function

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,44 +1,30 @@
 <?php
 
-// $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
-
-// $physicists_array = explode(', ', $physicists_string);
-
-// // change the last item to start with ' and $name'
-// $last_item = array_search('Tony Stark', $physicists_array);
-
-// $last_item = "and $physicists_array[$last_item]";
-
-// // switch the last item in physicists_array with $last_item
-// array_pop($physicists_array);
-
-// array_push($physicists_array, $last_item);
-
-// // imploding the array with the new $last_item edit
-// $famous_fake_physicists = implode(', ', $physicists_array);
-
-// echo $famous_fake_physicists . PHP_EOL;
-
-
 // Converts array into list n1, n2, ..., and n3
-function humanized_list($array) {
-	// Your solution goes here!
-	sort($array);
+function humanized_list($string, $x = false) 
+{
+	// explodes array to edit the last item in the string.
+	$array = explode(', ', $string);
+
+	// second parameter allows sorting to be optional
+	if($x === true)
+	{
+		sort($array);
+	}
+
+	// removes last item and replaces it with new item
 	$last_item = array_pop($array);
 	$last_item = "and $last_item";
 	array_push($array, $last_item);
+
+	// new string is returned outside the function
 	return implode(', ', $array);
 }
 
-// List of famous peeps
 $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
-$physicists_array = explode(', ', $physicists_string);
+$famous_fake_physicists = humanized_list($physicists_string);
 
-// Humanize that list
-$famous_fake_physicists = humanized_list($physicists_array);
-
-// Output sentence
 echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.";
 
 
